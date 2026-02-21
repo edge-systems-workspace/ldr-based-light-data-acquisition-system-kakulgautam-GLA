308564cbc77384f706c570a120cb350be3f998ed
kakul-gautam <gautamkakul5@gmail.com>
2026-02-21 20:27:41 +0530

    read LDR sensor value and print brightness status

--- src/main.cpp
+++ src/main.cpp
@@ -33,15 +33,24 @@ void loop() {
 
     // TODO 5:
     // Read analog value from LDR
+    ldrValue = analogRead(LDR_PIN);
 
     // TODO 6:
     // Print raw ADC value
+    Serial.print("LDR Raw Value: ");
+    Serial.println(ldrValue);
 
     // TODO 7:
     // Apply threshold logic (Bright / Dark detection)
+    if (ldrValue < 500) {   // Threshold value (adjust if needed)
 
     // TODO 8:
     // Print brightness status
+        Serial.println("Status: Bright Environment");
+    }
+    else {
+        Serial.println("Status: Dark Environment");
+    }
 
     // TODO 9:
     // Add delay (500ms or 1 second)
